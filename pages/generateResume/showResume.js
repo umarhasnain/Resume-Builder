de978e9c44464b8eac4c208409ea8aa116073794
Umar Hasnain <umarhasnain045@gmail.com>
2024-09-25 07:25:37 -0700

    changes

--- pages/generateResume/showResume.js
+++ pages/generateResume/showResume.js
@@ -1,10 +1,7 @@
 
-
-
-
 let btn = document.getElementById("btn");
 let showResume = document.getElementById("showResume");
-showResume.style.display = "block";
+showResume.style.display = "none";
 
 let showFunc = () => {
   let name = document.getElementById("name");
@@ -24,11 +21,8 @@ let showFunc = () => {
   let eduEndDate = document.getElementById("eduEndDate");
   // var todo_input = document.getElementById("todo_input");
   var skillArea = document.getElementById("skillArea");
-  let profile_img = document.getElementById("profile-img");
-
- const profileImgLink = profile_img.files[0];
+  var address = document.getElementById("address");
  
-  console.log(profileImgLink.name);
   
   //Show Reume Name:
   let showFulname = document.getElementById("showFulname");
@@ -47,13 +41,12 @@ let showFunc = () => {
   let showEduStartDate = document.getElementById("showEduStartDate");
   let showEduEndDate = document.getElementById("showEduEndDate");
   let skillList = document.getElementById("skillList");
-  let profileImg = document.getElementById("profileImg");
- console.log(profileImg);
+  let showAddress = document.getElementById("showAddress");
+
  
 
   showFulname.innerHTML = name.value;
   jobRole.innerHTML = job_rule.value;
-  profileImg.src.innerHTML = "profileImgLink.name"
   showPhoneNumber.innerHTML = phoneNumber.value;
   showGithub.innerHTML = github
   showLinkedin.innerHTML = linkedin.value;
@@ -68,44 +61,13 @@ let showFunc = () => {
   showEduStartDate.innerHTML = eduStartDate.value
   showEduEndDate.innerHTML = eduEndDate.value
   skillList.innerHTML = skillArea.value
+  showAddress.innerHTML = address.value
 
-//   showResume.style.display = "block";
+  showResume.style.display = "block";
 
-  console.log(name.value);
-  console.log(objective.value);
-  // console.log(skills.value);
-//   const newvalue = arr.map((item,i)=>{
-// console.log(item,i);
 
-//   })
-// console.log(arr);
-
-  // console.log(userNameText);
+  
 };
 
 btn.addEventListener("click", showFunc);
 
-
-let skillbtn = document.getElementById("btn");
-let skills = document.getElementById("skills");
-let input_list = document.getElementById("input_list");
-var arr = [];
-console.log(arr);
-
-
-function addSkill() {
-
-    var todo_input = document.getElementById("todo_input");
-    if (todo_input.value.trim() !== "") {
-        var li = `
-      <li> ${todo_input.value} 
-      </li>
-      `;
-        arr.push(todo_input.value);
-        localStorage.setItem("input_list", JSON.stringify(arr));
-        input_list.innerHTML += li;
-        todo_input.value = "";
-
-    }
-
-};
